93d9d521ee37b0d23bfde22054e49ee35f6a0a8c
Meerim <meerione11@gmail.com>
2022-02-18 12:01:15 +0600

    feat: finished

---
+++ 18-02/1/script.js
@@ -0,0 +1,47 @@
+let num = 10;
+let result4 = num++;
+console.log('result1: ' + num);
+console.log('result2: ' + result4);
+
+let h = 10;
+let res6 = ++h;
+console.log('result3: ' + h);
+console.log('result4: ' + res6);
+
+let num1 = 10;
+let result5 = num1--;
+console.log('result5: ' + num1);
+console.log('result6: ' + result5);
+
+let name = 'Meerim';
+console.log('Привет, ' + name + '!');
+
+let age = 22;
+console.log('Мне ' + age + ' лет!');
+
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result3 = c + d;
+console.log(result3);
+
+let num2 = 47;
+num2 += 7;
+alert(num2);
+
+let num3 = 47;
+num3 -= 18;
+alert(num3);
+
+let num4 = 47;
+num4 *= 10;
+alert(num4);
+
+let num5 = 47;
+num5 /= 15;
+alert(num5);
+
+
+
+
